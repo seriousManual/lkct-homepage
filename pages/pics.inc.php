cc948e4503c9849c95a538ad85fe57aafcf5b87c
zaphod1984 <zaphod84@gmx.de>
2013-10-16 22:14:24 +0200

    new stuff

--- pages/pics.inc.php
+++ pages/pics.inc.php
@@ -1,3 +1,8 @@
 <div id="pagepics" class="page">
-    pics
+    <h2>Pics</h2>
+
+    <a class="fancybox" rel="group" href="/pics/band/band1.jpg"><img src="/pics/band/t_band1.jpg" alt="" /></a>
+    <a class="fancybox" rel="group" href="/pics/band/band2.jpg"><img src="/pics/band/t_band2.jpg" alt="" /></a>
+    <a class="fancybox" rel="group" href="/pics/band/band3.jpg"><img src="/pics/band/t_band3.jpg" alt="" /></a>
+    <a class="fancybox" rel="group" href="/pics/band/band4.jpg"><img src="/pics/band/t_band4.jpg" alt="" /></a>
 </div>
