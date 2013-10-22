bee9481cd78214590557b60e9a0d6f51de837f97
zaphod1984 <zaphod84@gmx.de>
2013-10-23 00:39:51 +0200

    logo

--- pages/pics.inc.php
+++ pages/pics.inc.php
@@ -2,12 +2,16 @@
     <h2>Pics</h2>
 
     <h3>Band</h3>
-    <a class="fancybox" rel="group" href="/pics/band/band1.jpg"><img src="/pics/band/t_band1.jpg" alt="" /></a>
-    <a class="fancybox" rel="group" href="/pics/band/band2.jpg"><img src="/pics/band/t_band2.jpg" alt="" /></a>
-    <a class="fancybox" rel="group" href="/pics/band/band3.jpg"><img src="/pics/band/t_band3.jpg" alt="" /></a>
+    <a class="fancybox" rel="groupBand" href="/pics/band/band1.jpg"><img src="/pics/band/t_band1.jpg" alt="" /></a>
+    <a class="fancybox" rel="groupBand" href="/pics/band/band2.jpg"><img src="/pics/band/t_band2.jpg" alt="" /></a>
+    <a class="fancybox" rel="groupBand" href="/pics/band/band3.jpg"><img src="/pics/band/t_band3.jpg" alt="" /></a>
 
     <h3>Live</h3>
-    <a class="fancybox" rel="group" href="/pics/live/live1.jpg"><img src="/pics/live/t_live1.jpg" alt="" /></a>
-    <a class="fancybox" rel="group" href="/pics/live/live2.jpg"><img src="/pics/live/t_live2.jpg" alt="" /></a>
-    <a class="fancybox" rel="group" href="/pics/live/live4.jpg"><img src="/pics/live/t_live4.jpg" alt="" /></a>
+    <a class="fancybox" rel="groupLive" href="/pics/live/live1.jpg"><img src="/pics/live/t_live1.jpg" alt="" /></a>
+    <a class="fancybox" rel="groupLive" href="/pics/live/live2.jpg"><img src="/pics/live/t_live2.jpg" alt="" /></a>
+    <a class="fancybox" rel="groupLive" href="/pics/live/live4.jpg"><img src="/pics/live/t_live4.jpg" alt="" /></a>
+
+    <h3>Logo</h3>
+    <a class="fancybox" rel="groupLogo" href="/pics/logo/logo2.gif"><img src="/pics/logo/t_logo2.gif" alt="" /></a>
+    <a class="fancybox" rel="groupLogo" href="/pics/logo/logo1.gif"><img src="/pics/logo/t_logo1.gif" alt="" /></a>
 </div>
