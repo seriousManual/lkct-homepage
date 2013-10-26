d3f55683b9f5caf1546c2762d66e1b4668c8b35e
zaphod1984 <zaphod84@gmx.de>
2013-10-26 11:52:09 +0200

    script foo

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
+var catTriggered = false;
+
 $(document).ready(function() {
     $img = $('<img />').attr('src', 'cat_angry.jpg');
-
-    $('#cat').hover(function() {
-        $(this).css('background-image', 'url(cat_angry.jpg)');
-    }, function() {
-        $(this).css('background-image', 'url(cat.jpg)');
-    });
 
     $('#nav a').click(function() {
         var hash = $(this).attr('href').split('#')[1];
@@ -21,6 +17,16 @@ $(document).ready(function() {
     initial();
 
     $(".fancybox").fancybox();
+
+    $('#cat').hover(triggerCat.bind(null, true), triggerCat.bind(null, false));
+
+    setTimeout(function() {
+        if(catTriggered) return;
+
+        triggerCat(true);
+
+        setTimeout(triggerCat.bind(null, false), 500);
+    }, 300000 + parseInt(30000 * Math.random(), 10));
 });
 
 function initial() {
@@ -32,5 +38,15 @@ function initial() {
     } else {
         $('#pageband').show();
         $('#nav a[href=#band]').addClass('high');
+    }
+}
+
+function triggerCat(frightened) {
+    catTriggered = true;
+
+    if(frightened) {
+        $('#cat').css('background-image', 'url(cat_angry.jpg)');
+    } else {
+        $('#cat').css('background-image', 'url(cat.jpg)');
     }
 }
