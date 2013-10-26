38ee4cefcfcaf8d76a6758360337eec1d855f05a
zaphod1984 <zaphod84@gmx.de>
2013-10-26 13:29:44 +0200

    color of player changed to lkct green

--- pages/music.inc.php
+++ pages/music.inc.php
@@ -1,5 +1,5 @@
 <div id="pagemusic" class="page">
     <h2>Music</h2>
 
-    <iframe width="450" height="400" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Fusers%2F37122693&amp;color=ff6600&amp;auto_play=false&amp;show_artwork=false"></iframe>
+    <iframe width="450" height="400" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Fusers%2F37122693&amp;color=00ff00&amp;auto_play=false&amp;show_artwork=false"></iframe>
 </div>
