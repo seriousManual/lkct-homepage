d4084c7b1009a3baf2535af7153ee863ca195bcc
zaphod1984 <zaphod84@gmx.de>
2014-01-07 23:47:40 +0100

    reordered pics

--- pages/pics.inc.php
+++ pages/pics.inc.php
@@ -3,11 +3,11 @@
 
     <h3>Band</h3>
     <a class="fancybox" rel="groupBand" href="/pics/band/band1.jpg"><img src="/pics/band/t_band1.jpg" alt="" /></a>
+    <a class="fancybox" rel="groupBand" href="/pics/band/band6.jpg"><img src="/pics/band/t_band6.jpg" alt="" /></a>
     <a class="fancybox" rel="groupBand" href="/pics/band/band2.jpg"><img src="/pics/band/t_band2.jpg" alt="" /></a>
     <a class="fancybox" rel="groupBand" href="/pics/band/band3.jpg"><img src="/pics/band/t_band3.jpg" alt="" /></a>
     <a class="fancybox" rel="groupBand" href="/pics/band/band4.jpg"><img src="/pics/band/t_band4.jpg" alt="" /></a>
     <a class="fancybox" rel="groupBand" href="/pics/band/band5.jpg"><img src="/pics/band/t_band5.jpg" alt="" /></a>
-    <a class="fancybox" rel="groupBand" href="/pics/band/band6.jpg"><img src="/pics/band/t_band6.jpg" alt="" /></a>
 
     <h3>Live</h3>
     <a class="fancybox" rel="groupLive" href="/pics/live/live1.jpg"><img src="/pics/live/t_live1.jpg" alt="" /></a>
