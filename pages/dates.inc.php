a91d5894000482726b350d4c38d1945adc63fd94
zaphod1984 <zaphod84@gmx.de>
2014-02-25 09:09:12 +0100

    added highlighting of dates that happen today

--- pages/dates.inc.php
+++ pages/dates.inc.php
@@ -1,7 +1,7 @@
 <?php
     $dates = include('./inc/dates.inc.php');
 
-    function parseDates($dates, $when, $dir) {
+    function parseDates($dates, $when, $direction) {
         $now = new DateTime();
         $now->setTime(0,0,0);
         $res = Array();
@@ -11,13 +11,17 @@
                 $res[] = $date;
             }
 
-            if($date['date'] >= $now && $when == 'new') {
+            if($date['date'] > $now && $when == 'new') {
+                $res[] = $date;
+            }
+
+            if($date['date'] == $now && $when == 'today') {
                 $res[] = $date;
             }
         }
 
-        usort($res, function(Array $a, Array $b) use ($dir) {
-            return ($a['date']->getTimestamp() - $b['date']->getTimestamp()) * ($dir ? -1 : 1);
+        usort($res, function(Array $a, Array $b) use ($direction) {
+            return ($a['date']->getTimestamp() - $b['date']->getTimestamp()) * ($direction ? -1 : 1);
         });
 
         return $res;
@@ -25,37 +29,57 @@
 ?>
 
 <div id="pagedates" class="page">
-    <h2>Coming Up</h2>
+<?php
+    $parsedDates = parseDates($dates, 'today', false);
+
+    if(count($parsedDates) > 0) {
+        echo '<h2 class="dateToday">Today!</h2>';
+
+        echo "<ul class=\"dates\">";
+        foreach($parsedDates as $date) {
+            $a = $date['date'];
+
+            echo '<li class="clearfix">';
+            echo '<div class="date">' . $a->format('d.m.Y') . '</div>';
+            echo '<div class="desc">' . $date['desc'] . '</div>';
+            echo '</li>';
+        }
+        echo "</ul>";
+        echo "<br><br>";
+    }
+?>
+
+<h2>Coming Up</h2>
+<?php
+    $parsedDates = parseDates($dates, 'new', false);
+
+    if(count($parsedDates) > 0) {
+        echo "<ul class=\"dates\">";
+        foreach($parsedDates as $date) {
+            $a = $date['date'];
+
+            echo '<li class="clearfix">';
+            echo '<div class="date">' . $a->format('d.m.Y') . '</div>';
+            echo '<div class="desc">' . $date['desc'] . '</div>';
+            echo '</li>';
+        }
+        echo "</ul>";
+    } else {
+        echo '...no upcoming dates...';
+    }
+?>
+<br><br>
+<h2>Past Dates</h2>
+<ul class="dates past">
     <?php
-        $parsedDates = parseDates($dates, 'new', false);
+        foreach(parseDates($dates, 'past', true) as $date) {
+            $a = $date['date'];
 
-        if(count($parsedDates) > 0) {
-            echo "<ul class=\"dates\">";
-            foreach($parsedDates as $date) {
-                $a = $date['date'];
-
-                echo '<li class="clearfix">';
-                echo '<div class="date">' . $a->format('d.m.Y') . '</div>';
-                echo '<div class="desc">' . $date['desc'] . '</div>';
-                echo '</li>';
-            }
-            echo "</ul>";
-        } else {
-            echo '...no upcoming dates...';
+            echo '<li class="past clearfix">';
+            echo '<div class="date">' . $a->format('d.m.Y') . '</div>';
+            echo '<div class="desc">' . $date['desc'] . '</div>';
+            echo '</li>';
         }
     ?>
-    <br><br>
-    <h2>Past Dates</h2>
-    <ul class="dates past">
-        <?php
-            foreach(parseDates($dates, 'past', true) as $date) {
-                $a = $date['date'];
-
-                echo '<li class="past clearfix">';
-                echo '<div class="date">' . $a->format('d.m.Y') . '</div>';
-                echo '<div class="desc">' . $date['desc'] . '</div>';
-                echo '</li>';
-            }
-        ?>
-    </ul>
+</ul>
 </div>
