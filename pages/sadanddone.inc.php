cf82a9285d219279dcd0815ab9a98d9731016f0f
zaphod1984 <zaphod84@gmx.de>
2014-04-05 12:48:01 +0200

    added download link to sad page

--- pages/sadanddone.inc.php
+++ pages/sadanddone.inc.php
@@ -1,5 +1,7 @@
 <div id="pagesadanddone" class="page">
     <h2>Sad and Done</h2>
 
-    <iframe width="400" height="200" src="//www.youtube.com/embed/W_81WCR1WTQ" frameborder="0" allowfullscreen></iframe>
+    <iframe width="400" height="300" src="//www.youtube.com/embed/W_81WCR1WTQ" frameborder="0" allowfullscreen></iframe>
+
+    <a href="/download_sad/sad_and_done.mp3" class="sadDownload">Zum Download</a>
 </div>
