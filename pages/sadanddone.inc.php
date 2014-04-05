7c658f6e2097c7149ff3a5e0f0c89edbbe4d284b
zaphod1984 <zaphod84@gmx.de>
2014-04-05 12:52:53 +0200

    added download hint

--- pages/sadanddone.inc.php
+++ pages/sadanddone.inc.php
@@ -4,4 +4,5 @@
     <iframe width="400" height="300" src="//www.youtube.com/embed/W_81WCR1WTQ" frameborder="0" allowfullscreen></iframe>
 
     <a href="/download_sad/sad_and_done.mp3" class="sadDownload">Zum Download</a>
+    <div class="sadDownload_text">(Rechtsklick + "Speichern unter" zum downloaden)</div>
 </div>
